1562b53fb18ae770f315c053c67f6b6cd106f05c
Andrew Van Uitert <avanuitert@control4.com>
2017-01-29 14:52:47 -0700

    Just a few minor cleanup issues

--- src/org/usfirst/frc5933/Sonny/commands/DriveStraight.java
+++ src/org/usfirst/frc5933/Sonny/commands/DriveStraight.java
@@ -37,6 +37,7 @@ public class DriveStraight extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        setTimeout(5);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -46,15 +47,17 @@ public class DriveStraight extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
