ec97ee33698e6e8709c2378b1aeb36031b8ec81e
Andrew Van Uitert <avanuitert@control4.com>
2017-02-04 12:19:39 -0700

    Added prototype testing code

--- src/org/usfirst/frc5933/Sonny/commands/BackFlapOpen.java
+++ src/org/usfirst/frc5933/Sonny/commands/BackFlapOpen.java
@@ -60,5 +60,6 @@ public class BackFlapOpen extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
